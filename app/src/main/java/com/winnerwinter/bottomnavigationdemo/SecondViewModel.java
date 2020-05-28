75b4bc6daa28a6443774121aa3ce6f3c2c715345
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-05-28 22:19:50 +0800

    架构有了

---
+++ app/src/main/java/com/winnerwinter/bottomnavigationdemo/SecondViewModel.java
@@ -0,0 +1,7 @@
+package com.winnerwinter.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class SecondViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/winnerwinter/bottomnavigationdemo/ThirdViewModel.java
@@ -0,0 +1,7 @@
+package com.winnerwinter.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class ThirdViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
